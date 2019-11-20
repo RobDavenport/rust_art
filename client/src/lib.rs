b1e11bf61a76c2bf1741ec7a58de789eb5b31acc
Zerve0 <zerve0@hotmail.com>
2019-11-21 04:32:48 +0900

    WIP add smooth pixels

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -31,8 +31,9 @@ impl Pixel {
 #[repr(u8)]
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub enum GenerationMethod {
-    None = 0,
-    Random = 1,
+    None,
+    Random,
+    SmoothPixels
 }
 
 #[wasm_bindgen]
@@ -67,49 +68,42 @@ impl ColorDepthData {
             ColorDepth::Bit24 => ColorDepthData {
                 width: 4096,
                 height: 4096,
-                //contrast: 1,
                 steps: 255,
                 bitmask: 0b11111111_11111111_11111111_11111111,
             },
             ColorDepth::Bit21 => ColorDepthData {
                 width: 2048,
                 height: 1024,
-                //contrast: 2,
                 steps: 127,
                 bitmask: 0b11111111_01111111_01111111_01111111
             },
             ColorDepth::Bit18 => ColorDepthData {
                 width: 512,
                 height: 512,
-                //contrast: 4,
                 steps: 63, 
                 bitmask: 0b11111111_00111111_00111111_00111111
             },
             ColorDepth::Bit15 => ColorDepthData {
                 width: 256,
                 height: 128,
-                //contrast: 8,
                 steps: 31,
                 bitmask: 0b11111111_00011111_00011111_00011111
             },
             ColorDepth::Bit12 => ColorDepthData {
                 width: 64,
                 height: 64,
-                //contrast: 16,
                 steps: 15,
                 bitmask: 0b11111111_00001111_00001111_00001111
             },
             ColorDepth::Bit9 => ColorDepthData {
                 width: 32,
                 height: 16,
-                //contrast: 32,
                 steps: 7,
                 bitmask: 0b11111111_00000111_00000111_00000111
             },
             ColorDepth::Bit6 => ColorDepthData {
                 width: 8,
                 height: 8,
-                //contrast: 64,
                 steps: 3,
                 bitmask: 0b11111111_00000011_00000011_00000011
             },
@@ -168,6 +162,26 @@ fn default_pixels(color_depth_data: &ColorDepthData) -> Vec<Pixel> {
     return pixels;
 }
 
+//TODO: This function
+fn smooth_pixels(color_depth_data: &ColorDepthData) -> Vec<Pixel> {
+    let mut pixels: Vec<Pixel> = Vec::new();
+    let steps = color_depth_data.steps;
+    let contrast = 255.0 / steps as f32;
+
+    for r in 0..=steps {
+        for g in 0..=steps {
+            for b in 0..=steps {
+                let pr = (contrast * r as f32).floor() as u8;
+                let pg = (contrast * 0 as f32).floor() as u8;
+                let pb = (contrast * 0 as f32).floor() as u8;
+                pixels.push(Pixel::new(pr, pg, pb as u8));
+            }
+        }
+    }
+    
+    return pixels;
+}
+
 fn random_pixels(color_depth_data: &ColorDepthData) -> Vec<Pixel> {
     let mut rng = thread_rng();
     let mut pixels = default_pixels(color_depth_data);
@@ -178,7 +192,8 @@ fn random_pixels(color_depth_data: &ColorDepthData) -> Vec<Pixel> {
 
 fn generate_pixels(gen: GenerationMethod, color_depth_data: &ColorDepthData) -> Vec<Pixel> {
     match gen {
+        GenerationMethod::None => default_pixels(color_depth_data),
         GenerationMethod::Random => random_pixels(color_depth_data),
-        _ => default_pixels(color_depth_data)
+        GenerationMethod::SmoothPixels => smooth_pixels(color_depth_data)
     }
 }
